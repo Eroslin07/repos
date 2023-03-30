bd2adc3436110811189566c684780f6bbaf11693
long.he <long.he@newtouch.cn>
2023-03-30 15:13:58 +0800

    解决打包问题、添加契约锁封装模块目录

---
+++ uctp-framework/uctp-spring-boot-starter-biz-qiyuesuo/src/main/java/com/newtouch/uctp/framework/qiyuesuo/packageinfo.java
@@ -0,0 +1,11 @@
+package com.newtouch.uctp.framework.qiyuesuo;
+
+/**
+ * @author helong
+ * @date 2023/3/30 11:21
+ */
+public class packageinfo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ uctp-framework/uctp-spring-boot-starter-biz-qiyuesuo/src/test/java/com/newtouch/uctp/framework/qiyuesuo/UctpSpringBootStarterBizQiyuesuoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newtouch.uctp.framework.qiyuesuo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UctpSpringBootStarterBizQiyuesuoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
