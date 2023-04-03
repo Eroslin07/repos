63795104e647510fe628719bfabe8192f8076696
long.he <long.he@newtouch.cn>
2023-04-03 19:24:29 +0800

    MinIO文件服务器上传根路径定义

---
+++ uctp-api/uctp-api-business/src/main/java/com/newtouch/uctp/module/business/enums/UploadConstants.java
@@ -0,0 +1,25 @@
+package com.newtouch.uctp.module.business.enums;
+
+/**
+ * MinIO文件服务器上传根路径定义
+ * @author helong
+ * @date 2023/4/3 19:17
+ */
+public class UploadConstants {
+    /**
+     * 身份证上传路径
+     */
+    public static final String IDCARD = "idcard";
+    /**
+     * 营业执照上传路径
+     */
+    public static final String BUSINESS_LICENSE = "business_license";
+    /**
+     * 行驶证上传路径
+     */
+    public static final String VEHICLE_LICENSE = "vehicle_license";
+    /**
+     * 机动车登记证书上传路径
+     */
+    public static final String VEHICLE_REGISTRATION_CERTIFICATE = "vehicle_registration_certificate";
+}
