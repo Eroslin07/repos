4e355a5bf185f08d9758c59fbcb05e30031b7880
刘健 <jian.liu6@newtouch.com>
2023-04-13 10:14:55 +0800

    商户账户管理

---
+++ uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/controller/app/account/AccountController.java
@@ -0,0 +1,27 @@
+package com.newtouch.uctp.module.business.controller.app.account;
+
+import com.newtouch.uctp.framework.common.pojo.CommonResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.newtouch.uctp.framework.common.pojo.CommonResult.success;
+
+@Tag(name = "商户账户")
+@RestController
+@RequestMapping("/uctp/account")
+@Validated
+public class AccountController {
+
+    @PostMapping("/open")
+    @Operation(summary = "商户账户开户")
+    public CommonResult<String> accountOpen() {
+        return success("OK");
+    }
+
+
+}
