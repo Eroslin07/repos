8513592b614b2be5144f9029402bfa106b3c9556
luo_cong <cong.luo@newtouch.com>
2023-04-20 20:05:08 +0800

    修复打包

--- uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/dal/mysql/user/UserMapper.java
+++ uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/dal/mysql/user/UserMapper.java
@@ -2,7 +2,7 @@ package com.newtouch.uctp.module.business.dal.mysql.user;
 
 
 import com.newtouch.uctp.framework.mybatis.core.mapper.BaseMapperX;
-import com.newtouch.uctp.module.bpm.dal.dataobject.user.AdminUserDO;
+import com.newtouch.uctp.module.business.dal.dataobject.user.AdminUserDO;
 import org.apache.ibatis.annotations.Mapper;
 
 
