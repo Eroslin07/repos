e00938e96de121dee55a72adcb0d0c7fedc2ca2f
刘健 <jian.liu6@newtouch.com>
2023-04-25 13:10:11 +0800

    商户账户开户Feign代码

---
+++ uctp-api/uctp-api-business/src/main/java/com/newtouch/uctp/module/business/api/account/dto/AccountDTO.java
@@ -0,0 +1,31 @@
+package com.newtouch.uctp.module.business.api.account.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Schema(description = "商户开立虚拟子账户号")
+@Data
+public class AccountDTO {
+
+    @Schema(description = "租户ID")
+    @NotNull(message = "租户ID不能为空")
+    private Long tenantId;
+
+    @Schema(description = "商户ID")
+    @NotNull(message = "商户ID不能为空")
+    private Long merchantId;
+
+    @Schema(description = "开户行名称")
+    @NotNull(message = "开户行名称不能为空")
+    private String bankName;
+
+    @Schema(description = "对公银行账号")
+    @NotNull(message = "对公银行账号不能为空")
+    private String bankNo;
+
+    @Schema(description = "保证金充值银行账号")
+    @NotNull(message = "保证金充值银行账号不能为空")
+    private String cashBankNo;
+}
