2f1f965e5fdd80e31f6e4149fd8bd85590b982e3
wss <yangyuyuan111@163.com>
2023-05-06 18:41:21 +0800

    图片放到分包内

--- uctp-app/subPages/common/abnormaPage/index.vue
+++ uctp-app/subPages/common/abnormaPage/index.vue
@@ -21,23 +21,23 @@
 			<view>系统异常</view>
 		</view>
 		<view v-if="isSHowTip=='signing'" class="tips">
-			<image src="/static/images/bycar/signing.gif" mode=""></image>
+			<image src="@/subPages/static/images/bycar/signing.gif" mode=""></image>
 			<view class="contract-tips">合同签署中...</view>
 		</view>
 		<view v-if="isSHowTip=='creating'" class="tips">
-			<image src="/static/images/bycar/creating.gif" mode="widthFix" ></image>
+			<image src="@/subPages/static/images/bycar/creating.gif" mode="widthFix" ></image>
 			<view class="contract-tips">合同生成中...</view>
 		</view>
 		<view v-if="isSHowTip=='createFail'" class="tips">
-			<image src="/static/images/bycar/createFail.ico" mode="widthFix" ></image>
+			<image src="@/subPages/static/images/bycar/createFail.ico" mode="widthFix" ></image>
 			<view class="contract-tips">合同生成失败 ！</view>
 		</view>
 		<view v-if="isSHowTip=='signFail'" class="tips">
-			<image src="/static/images/bycar/signFail.ico" mode="widthFix" ></image>
+			<image src="@/subPages/static/images/bycar/signFail.ico" mode="widthFix" ></image>
 			<view class="contract-tips">抱歉,电子合同签属失败 ！</view>
 		</view>
 		<view v-if="isSHowTip=='signSuccess'" class="tips">
-			<image src="/static/images/bycar/signSuccess.ico" mode="widthFix" ></image>
+			<image src="@/subPages/static/images/bycar/signSuccess.ico" mode="widthFix" ></image>
 			<view class="contract-tips suss-tips">恭喜您，电子合同签属成功！</view>
 		</view>
 	</view>
