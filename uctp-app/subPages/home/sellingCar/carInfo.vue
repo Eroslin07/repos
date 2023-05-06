15977aeab1a73fa63b06bf2c12682cb17beb5df4
chentangyu <tangyu.chen@newtouch.com>
2023-05-06 16:33:07 +0800

    页面优化

--- uctp-app/subPages/home/sellingCar/carInfo.vue
+++ uctp-app/subPages/home/sellingCar/carInfo.vue
@@ -169,8 +169,8 @@
 							:text="'公允值范围：'+fairValue.value1+'万元-'+fairValue.value2+'万元'" color="#e26e1f"></u--text>
 						<view v-if="fairStatus == '不通过'" style="margin-left: 15px;color: #e26e1f;">公允价值审核-退回 ></view>
 						<view style="margin-left: 15px;color: #e26e1f;">
-							预计费用{{sellerForm.total}}元，利润{{sellerForm.profit}}元。<text @click="handleDetail">明细请查看
-								></text></view>
+							预计费用{{sellerForm.total}}元，利润{{sellerForm.profit}}元。<text @click="handleDetail">明细请查看 ></text>
+						</view>
 					</view>
 					<u-form-item label="收款方式" :required="true" prop="sellType" borderBottom>
 						<u-radio-group v-model="sellerForm.sellType" placement="row" activeColor="#fd6404">
