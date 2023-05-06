fb44834bdfc08ef22ce4552ddc0aed687556475a
chentangyu <tangyu.chen@newtouch.com>
2023-05-06 10:03:26 +0800

    配置修改

--- uctp-app/config.js
+++ uctp-app/config.js
@@ -3,7 +3,7 @@ module.exports = {
   baseUrl: 'https://miniapp.wanguo.net:28001/',
   // 上传路径
   uploadUrl: 'https://miniapp.wanguo.net:28001/app-api/infra/file/upload',
-  upLoadTestUrl:'http://miniapp.wanguo.net:28001/app-api/infra/file/uploadReport',
+  upLoadTestUrl:'https://miniapp.wanguo.net:28001/app-api/infra/file/uploadReport',
 
   baseApi: '/app-api',
   // 应用信息
