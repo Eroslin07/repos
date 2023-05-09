ec62f90c3a501d868195152e5423721d571b2c47
zhangjun <1548389528@qq.com>
2023-05-09 14:46:40 +0800

    去除主启动类的openfeignclients

--- uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/UctpBusinessApplication.java
+++ uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/UctpBusinessApplication.java
@@ -2,11 +2,8 @@ package com.newtouch.uctp.module.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class UctpBusinessApplication {
 
 	public static void main(String[] args) {
