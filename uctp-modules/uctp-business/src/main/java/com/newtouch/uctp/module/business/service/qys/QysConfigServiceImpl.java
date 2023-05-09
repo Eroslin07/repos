1532b51f451e380400cc2da18bfbcc63c93905a4
luo_cong <cong.luo@newtouch.com>
2023-05-09 20:47:37 +0800

    契约锁

--- uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/service/qys/QysConfigServiceImpl.java
+++ uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/service/qys/QysConfigServiceImpl.java
@@ -378,10 +378,7 @@ public class QysConfigServiceImpl implements QysConfigService {
                                 Boolean.FALSE);
                         break;
                     case SIGNING:
-                        //如果是个人签署，进行企业静默签章
-                        if (ObjectUtil.equals("tenantType",contractStatusDTO.getTenantType())) {
-                            this.companySign(contractDO.getContractId());
-                        }
+
                         break;
                 }
             } else if (contractDO.getContractType().equals(2)) {
@@ -412,8 +409,10 @@ public class QysConfigServiceImpl implements QysConfigService {
                                 Boolean.FALSE);
                         break;
                     case SIGNING:
-                        //签署中需要进行下一个企业静默签章
-//                        this.companySign(contractDO.getContractId());
+                        //如果是个人签署，进行企业静默签章
+                        if (ObjectUtil.equals("PERSONAL",contractStatusDTO.getTenantType())) {
+                            this.companySign(contractDO.getContractId());
+                        }
                         break;
                 }
             } else if (contractDO.getContractType().equals(3)) {
