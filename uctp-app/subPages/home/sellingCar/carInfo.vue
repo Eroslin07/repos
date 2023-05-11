45362423c48a595006561905ece669d3ff64c443
chentangyu <tangyu.chen@newtouch.com>
2023-05-11 18:56:43 +0800

    合同顺序调整

--- uctp-app/subPages/home/sellingCar/carInfo.vue
+++ uctp-app/subPages/home/sellingCar/carInfo.vue
@@ -28,18 +28,18 @@
 				<view class="look-over">
 					<u-grid col="2">
 						<u-grid-item>
-							<uni-card @click="handleCollection(2)">
-								<view style="color: #333;font-size: 28rpx;">
-									<image src="../../../static/images/bycar/hetong3.png" class="form-image"></image>
-									<text>收车合同</text>
-								</view>
-							</uni-card>
-						</u-grid-item>
-						<u-grid-item>
 							<uni-card @click="handleCollection(1)">
 								<view style="color: #333;font-size: 28rpx;">
 									<image src="../../../static/images/bycar/hetong3.png" class="form-image"></image>
 									<text>委托合同</text>
+								</view>
+							</uni-card>
+						</u-grid-item>
+						<u-grid-item>
+							<uni-card @click="handleCollection(2)">
+								<view style="color: #333;font-size: 28rpx;">
+									<image src="../../../static/images/bycar/hetong3.png" class="form-image"></image>
+									<text>收车合同</text>
 								</view>
 							</uni-card>
 						</u-grid-item>
