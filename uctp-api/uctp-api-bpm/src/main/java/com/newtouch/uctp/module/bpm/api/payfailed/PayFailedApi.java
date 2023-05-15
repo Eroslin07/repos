d1b13445205f951d137deba4ed8556e0f73229f7
long.he <long.he@newtouch.cn>
2023-05-15 15:27:44 +0800

    支付失败流程发起

--- uctp-api/uctp-api-bpm/src/main/java/com/newtouch/uctp/module/bpm/api/payfailed/PayFailedApi.java
+++ uctp-api/uctp-api-bpm/src/main/java/com/newtouch/uctp/module/bpm/api/payfailed/PayFailedApi.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newtouch.uctp.framework.common.pojo.CommonResult;
 import com.newtouch.uctp.module.bpm.api.payfailed.dto.PayFailedCreateBpmDTO;
@@ -25,4 +26,9 @@ public interface PayFailedApi {
     @PostMapping(PREFIX + "/create")
     @Operation(summary = "收车支付失败时，后台调用自动创建[收车款支付失败流程]")
     public CommonResult<String> createBpm(@Valid @RequestBody PayFailedCreateBpmDTO createReqVO);
+
+    @PostMapping(PREFIX + "/createBpm")
+    @Operation(summary = "根据（契约锁）收车合同ID、支付失败原因发起支付失败流程")
+    public CommonResult<String> createPayFailedBpm(@RequestParam("contractId") Long contractId,
+                                          @RequestParam(name = "failReason", required = false, defaultValue = "支付侧支付失败") String failReason);
 }
--- uctp-modules/uctp-bpm/src/main/java/com/newtouch/uctp/module/bpm/service/payfailed/PayFailedServiceImpl.java
+++ uctp-modules/uctp-bpm/src/main/java/com/newtouch/uctp/module/bpm/service/payfailed/PayFailedServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.util.ObjectUtil;
 import io.seata.spring.annotation.GlobalTransactional;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -19,7 +20,9 @@ import com.newtouch.uctp.framework.web.core.util.WebFrameworkUtils;
 import com.newtouch.uctp.module.bpm.dal.dataobject.car.ContractDO;
 import com.newtouch.uctp.module.bpm.dal.mysql.car.CarInfoMapper;
 import com.newtouch.uctp.module.bpm.dal.mysql.car.ContractMapper;
+import com.newtouch.uctp.module.bpm.enums.definition.BpmDefTypeEnum;
 import com.newtouch.uctp.module.bpm.service.task.BpmProcessInstanceService;
+import com.newtouch.uctp.module.business.api.carInfo.CarInfoApi;
 import com.newtouch.uctp.module.business.api.transfer.CarTransferApi;
 import com.newtouch.uctp.module.system.api.dept.DeptApi;
 import com.newtouch.uctp.module.system.api.dept.dto.DeptRespDTO;
@@ -51,22 +54,24 @@ public class PayFailedServiceImpl implements PayFailedService {
     private BpmProcessInstanceService processInstanceService;
     @Resource
     private ContractMapper contractMapper;
+    @Resource
+    private CarInfoApi carInfoApi;
 
     @Override
     @GlobalTransactional
     @Transactional(rollbackFor = Exception.class)
     public String createBpm(Long contractId, String procDefKey, Map<String, Object> variables) {
         if (ObjectUtil.isNull(contractId) || !StringUtils.hasText(procDefKey)) {
-            throw new IllegalArgumentException("合同号ID、流程业务标识不能为空。");
+            throw new RuntimeException("合同号ID、流程业务标识不能为空。");
         }
         ContractDO contractDO = contractMapper.selectByContractIdAndContractType(contractId, 2);
         if (ObjectUtil.isNull(contractDO) || ObjectUtil.isNull(contractDO.getId())) {
-            throw new IllegalArgumentException("根据合同号ID查询车辆合同信息失败，无法发起收车支付失败流程。");
+            throw new RuntimeException("根据合同号ID查询车辆合同信息失败，无法发起收车支付失败流程。");
         }
         Long tenantId = contractDO.getTenantId();
         String creator = contractDO.getCreator();
         if (ObjectUtil.isNull(tenantId) || !StringUtils.hasText(creator)) {
-            throw new IllegalArgumentException("根据合同号ID获取合同创建人或对应租户信息失败，请检查合同信息。");
+            throw new RuntimeException("根据合同号ID获取合同创建人或对应租户信息失败，请检查合同信息。");
         }
         AtomicReference<String> formMainId = new AtomicReference<>("");
         TenantUtils.execute(tenantId, () -> {
@@ -81,6 +86,47 @@ public class PayFailedServiceImpl implements PayFailedService {
 
             formMainId.set(processInstanceService.createProcessInstanceByKey(adminUserRespDTO.getId(), procDefKey, variables));
         });
+
+        return formMainId.get();
+    }
+
+    @Override
+    @GlobalTransactional
+    @Transactional(rollbackFor = Exception.class)
+    public String createBpm(Long contractId, String failReason) {
+        // 合同类型：1-收车委托合同   2-收车合同  3-卖车委托合同  4-卖车合同
+        ContractDO contractDO = contractMapper.selectByContractIdAndContractType(contractId, 2);
+        if (ObjectUtil.isNull(contractDO) || ObjectUtil.isNull(contractDO.getCarId())) {
+            throw new RuntimeException("获取（契约锁）收车合同【" + contractId + "】的基本信息失败。");
+        }
+        Long tenantId = contractDO.getTenantId();
+        String creator = contractDO.getCreator();
+        if (ObjectUtil.isNull(tenantId) || !StringUtils.hasText(creator)) {
+            throw new RuntimeException("根据合同号ID获取合同创建人或对应租户信息失败，请检查合同信息。");
+        }
+        AtomicReference<String> formMainId = new AtomicReference<>("");
+        TenantUtils.execute(tenantId, () -> {
+            WebFrameworkUtils.setLoginUserId(WebFrameworkUtils.getRequest(), Long.valueOf(creator));
+            TenantRespDTO tenantRespDTO = tenantApi.getTenant(tenantId).getCheckedData();
+            AdminUserRespDTO adminUserRespDTO = adminUserApi.getUser(Long.valueOf(creator)).getCheckedData();
+            DeptRespDTO deptRespDTO = deptApi.getDept(adminUserRespDTO.getDeptId()).getCheckedData();
+            Map<String, Object> formInfo = carInfoApi.getPayFailedCreateBpmInfo(contractId).getCheckedData();
+            Map<String, Object> variables = new HashMap<String, Object>();
+            variables.put("startUserId", adminUserRespDTO.getId());
+            variables.put("marketName", tenantRespDTO.getName());
+            variables.put("merchantName", deptRespDTO.getName());
+            variables.put("contractCode", contractDO.getCode());
+            HashMap<String, Object> formDataJsonVariable = new HashMap<String, Object>();
+            Map<String, Object> formMainDataObject = new HashMap<>();
+            formMainDataObject.put("merchantId", deptRespDTO.getId());
+            formMainDataObject.put("thirdId", contractId);
+            formMainDataObject.put("formDataJson", formInfo);
+            formDataJsonVariable.put("formMain", formMainDataObject);
+            variables.put("formDataJson", formDataJsonVariable);
+
+            formMainId.set(processInstanceService.createProcessInstanceByKey(adminUserRespDTO.getId(), BpmDefTypeEnum.SKZH.name(), variables));
+        });
+
 
         return formMainId.get();
     }
