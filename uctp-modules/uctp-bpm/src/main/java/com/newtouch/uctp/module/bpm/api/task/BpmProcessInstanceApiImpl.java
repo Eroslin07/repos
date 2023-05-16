b78c1654fb5b800316fa1d06f5cd68aceaa771f5
long.he <long.he@newtouch.cn>
2023-05-16 10:29:27 +0800

    利润提现

--- uctp-modules/uctp-bpm/src/main/java/com/newtouch/uctp/module/bpm/api/task/BpmProcessInstanceApiImpl.java
+++ uctp-modules/uctp-bpm/src/main/java/com/newtouch/uctp/module/bpm/api/task/BpmProcessInstanceApiImpl.java
@@ -3,8 +3,9 @@ package com.newtouch.uctp.module.bpm.api.task;
 import javax.annotation.Resource;
 import javax.validation.Valid;
 
-import org.springframework.stereotype.Service;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.newtouch.uctp.framework.common.pojo.CommonResult;
 import com.newtouch.uctp.module.bpm.api.task.dto.BpmProcessInstanceByKeyReqDTO;
@@ -12,6 +13,7 @@ import com.newtouch.uctp.module.bpm.api.task.dto.BpmProcessInstanceCreateReqDTO;
 import com.newtouch.uctp.module.bpm.service.task.BpmProcessInstanceService;
 
 import static com.newtouch.uctp.framework.common.pojo.CommonResult.success;
+import static com.newtouch.uctp.module.system.enums.ApiConstants.VERSION;
 
 /**
  * Flowable 流程实例 Api 实现类
@@ -19,7 +21,8 @@ import static com.newtouch.uctp.framework.common.pojo.CommonResult.success;
  * @author 芋道源码
  * @author jason
  */
-@Service
+@RestController // 提供 RESTful API 接口，给 Feign 调用
+@DubboService(version = VERSION) // 提供 Dubbo RPC 接口，给 Dubbo Consumer 调用
 @Validated
 public class BpmProcessInstanceApiImpl implements BpmProcessInstanceApi {
 
