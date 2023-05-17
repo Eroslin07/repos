390f751e5e9dbcb07521a1f8584fdc91dcfd7237
yr <jingjing.qin@newtouch.com>
2023-05-17 16:31:40 +0800

    问题

--- uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/dal/mysql/user/UserExtMapper.java
+++ uctp-modules/uctp-business/src/main/java/com/newtouch/uctp/module/business/dal/mysql/user/UserExtMapper.java
@@ -2,6 +2,7 @@ package com.newtouch.uctp.module.business.dal.mysql.user;
 
 
 import com.newtouch.uctp.framework.mybatis.core.mapper.BaseMapperX;
+import com.newtouch.uctp.framework.mybatis.core.query.QueryWrapperX;
 import com.newtouch.uctp.module.business.dal.dataobject.user.AdminUserDO;
 import com.newtouch.uctp.module.business.dal.dataobject.user.UserExtDO;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,7 +15,9 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserExtMapper extends BaseMapperX<UserExtDO> {
 
 
-
+    default int deleteByUserId(Long userId){
+        return delete(new QueryWrapperX<UserExtDO>().eq("USER_ID",userId));
+    }
 
 
 }
